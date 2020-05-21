b3c17a24568ac6e4055eb3615d06e59cdf0a3d8d
asda488 <39116917+asda488@users.noreply.github.com>
2020-05-21 16:21:11 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,23 @@
-//placeholder
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+var walking = false; 
+//techno locations
+var technowalkfront = [[39, 36, 74, 126], [135, 35, 75, 127], [230, 35, 76, 127]];  //113 162, 210 162, 306, 162
+function main() {
+  loadImages()
+}
+
+function loadImages() {
+	technospritesone = new Image();
+  technospritesone.src('img/technosprites2.png') //Load his sprites
+
+}
+
+
+function drawTechno() {
+	var index;
+	while(walking) { //carry on here tommorrow
+  	
+  }
+} 
