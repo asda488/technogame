cee05e8f58c3454cece14ad4d38eec19550675d4
asda488 <39116917+asda488@users.noreply.github.com>
2020-05-22 15:02:04 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -4,19 +4,24 @@ var situation = "walking" //starts walking
 //indexes
 var lastfunction = ""
 var speedcounter = 0
-var walkindex = 0; //i need to do something with this
-
+var walkindex = 0; 
 //keybooleans
 var rightPressed = false;
 var leftPressed = false;
 var downPressed = false;
 var upPressed = false;
 
-//techno locations
+//techno sprite locations fix left and right to center
 var technowalkfront = [[39, 36, 74, 126], [135, 35, 75, 127], [230, 35, 76, 127]];  //113 162, 210 162, 306 162
+var technowalkback = [[38, 179, 76, 127], [134, 179, 76, 127], [230, 179, 76, 127]]; //114 306, 210 306, 306 306
+var technowalkleft = [[26, 323, 76, 127], [110, 323, 76, 127], [191, 323, 76, 127]]; //93 450, 177 450, 258 450
+var technowalkright = [[35, 467, 76, 127], [116, 467, 76, 127], [200, 467, 76, 127]]; //102 594, 183 594, 267 594
+var technoidlefront = [[368, 26, 76, 133],[464, 26, 76, 133],[368, 170, 76, 133],[464, 170, 76, 133],[368, 314, 76, 133],[464, 314, 76, 133]]; //444 159, 540 159, 444 303, 540 303, 444 447, 540 447
+var technoidleback = [[368, 458, 76, 133], [464, 458, 76, 133]]; //444 591, 540 591
 
-
-//keyhandlers
+//keyhandlers and listeners
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 function keyDownHandler(e) {
         if(e.keyCode == 39 || e.keycode == 68) {
             rightPressed = true;
@@ -50,11 +55,11 @@ function keyDownHandler(e) {
 function main() {
   ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); //clear screen
   if (situation == "walking") {
-    drawTechno()
-    drawLand()
+    drawTechno();
+    drawLand();
   } //add else if
-  speedcounter++; //tick the speed counter, very important
-  requestAnimationFrame(main)
+  speedcounter = (speedcounter+1)%60; //tick the speed counter, very important
+  requestAnimationFrame(main);
 }
 
 function loadAssets() {
@@ -71,18 +76,62 @@ function drawLand(){
 }
 
 function technoWalk(){
-  //fix this to multi direction and location
-  if (lastfunction == "technoWalkfront") {
-    console.log("hi")
-  	ctx.drawImage(technospritesone, technowalkfront[walkindex][0], technowalkfront[walkindex][1], technowalkfront[walkindex][2], technowalkfront[walkindex][3], 0, 0, technowalkfront[walkindex][2], technowalkfront[walkindex][3]);
-  } else {
-  	walkindex = 0;
-    ctx.drawImage(technospritesone, technowalkfront[walkindex][0], technowalkfront[walkindex][1], technowalkfront[walkindex][2], technowalkfront[walkindex][3], 0, 0, technowalkfront[walkindex][2], technowalkfront[walkindex][3]);
+  //fix this to multi direction and location and modular
+  if (downPressed) {
+    if (lastfunction != "technoWalkfront") {
+  	  walkindex = 0;	
+    }
+	ctx.drawImage(technospritesone, technowalkfront[walkindex][0], technowalkfront[walkindex][1], technowalkfront[walkindex][2], technowalkfront[walkindex][3], 0, 0, technowalkfront[walkindex][2], technowalkfront[walkindex][3]);
+  lastfunction = "technoWalkfront";
+  if (speedcounter%12 === 0){
+    walkindex = (walkindex+1)%3;
   }
-   console.log(walkindex)
-  lastfunction = "technoWalkfront"
-  if (speedcounter%8 === 0){
-    	walkindex = (walkindex+1)%3;
+  } else if (upPressed) {
+      if (lastfunction != "technoWalkback") {
+  	    walkindex = 0;	
+    }
+	ctx.drawImage(technospritesone, technowalkback[walkindex][0], technowalkback[walkindex][1], technowalkback[walkindex][2], technowalkback[walkindex][3], 0, 0, technowalkback[walkindex][2], technowalkback[walkindex][3]);
+  lastfunction = "technoWalkback";
+  if (speedcounter%12 === 0){
+    walkindex = (walkindex+1)%3;
+  }
+  } else if (leftPressed) {
+      if (lastfunction != "technoWalkleft") {
+  	    walkindex = 0;	
+    }
+	ctx.drawImage(technospritesone, technowalkleft[walkindex][0], technowalkleft[walkindex][1], technowalkleft[walkindex][2], technowalkleft[walkindex][3], 0, 0, technowalkleft[walkindex][2], technowalkleft[walkindex][3]);
+  lastfunction = "technoWalkleft";
+  if (speedcounter%12 === 0){
+    walkindex = (walkindex+1)%3;
+  }
+  } else if (rightPressed) {
+      if (lastfunction != "technoWalkright") {
+    	  walkindex = 0;	
+    }
+	ctx.drawImage(technospritesone, technowalkright[walkindex][0], technowalkright[walkindex][1], technowalkright[walkindex][2], technowalkright[walkindex][3], 0, 0, technowalkright[walkindex][2], technowalkright[walkindex][3]);
+  lastfunction = "technoWalkright";
+  if (speedcounter%12 === 0){
+    walkindex = (walkindex+1)%3;
+  }
+  } else {
+    if (lastfunction != "technoIdlefront" && lastfunction != "technoIdleback"){
+      walkindex = 0;
+    }
+    if (lastfunction == "technoWalkback" || lastfunction == "technoIdleback") { //were you facing back or front?
+      ctx.drawImage(technospritesone, technoidleback[walkindex][0], technoidleback[walkindex][1], technoidleback[walkindex][2], technoidleback[walkindex][3], 0, 0, technoidleback[walkindex][2], technoidleback[walkindex][3]);
+      lastfunction = "technoIdleback";
+      if (speedcounter%12 === 0){
+        console.log(walkindex);
+        walkindex = (walkindex+1)%2;
+      }
+    } else {
+      ctx.drawImage(technospritesone, technoidlefront[walkindex][0], technoidlefront[walkindex][1], technoidlefront[walkindex][2], technoidlefront[walkindex][3], 0, 0, technoidlefront[walkindex][2], technoidlefront[walkindex][3]);
+      lastfunction = "technoIdlefront";
+      if (speedcounter%12 === 0){
+        console.log(walkindex);
+        walkindex = (walkindex+1)%6;
+      }
+    }
   }
 }
 
