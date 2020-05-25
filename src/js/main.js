f862c7d9bb2001030d6f9bbe3836223a7758e389
asda488 <39116917+asda488@users.noreply.github.com>
2020-05-25 10:07:02 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -19,35 +19,8 @@ var technowalkfront = [[39, 36, 74, 126], [135, 35, 75, 127], [230, 35, 76, 127]
 var technowalkback = [[38, 179, 76, 127], [134, 179, 76, 127], [230, 179, 76, 127]]; //114 306, 210 306, 306 306
 var technowalkleft = [[26, 323, 76, 127], [110, 323, 76, 127], [191, 323, 76, 127]]; //93 450, 177 450, 258 450
 var technowalkright = [[35, 467, 76, 127], [116, 467, 76, 127], [200, 467, 76, 127]]; //102 594, 183 594, 267 594
-var technoidlefront = [[368, 26, 76, 133],[464, 26, 76, 133],[368, 170, 76, 133],[464, 170, 76, 133],[368, 314, 76, 133],[464, 314, 76, 133]]; //444 159, 540 159, 444 303, 540 303, 444 447, 540 447
+var technoidlefront = [[368, 26, 76, 133], [464, 26, 76, 133], [368, 170, 76, 133], [464, 170, 76, 133],[368, 314, 76, 133],[464, 314, 76, 133]]; //444 159, 540 159, 444 303, 540 303, 444 447, 540 447
 var technoidleback = [[368, 458, 76, 133], [464, 458, 76, 133]]; //444 591, 540 591
-
-//terrain locations
-var postup = [56, 83, 31, 109]; //87 192
-var postright = [113, 86, 100, 58]; //213 144
-var postleft = [113, 182, 100, 58]; //213 240
-var post = [56, 230, 31, 58]; //87 288
-
-var fieldone = [44, 380, 112, 31]; //fields in reading order 156 411
-var fieldtwo = [167, 380, 112, 31]; //279 411
-var fieldthree = [44, 422, 112, 31]; //156 453
-var fieldfour = [170, 422, 112, 31]; //282 450
-var fieldlargeone = [47, 497, 112, 76]; //159 573
-var fieldlargetwo = [176, 497, 112, 76]; //288 573
-
-var bushsmall = [251, 83, 67, 61]; //318 144
-var bushmed = [356, 74, 97, 70]; //453 144
-var bushlarge = [287, 173, 154, 112]; //441 285
-var tree = [482, 47, 190, 256]; //672 303
-
-var stonechiselpillar = [483, 377, 39, 76]; //522 453
-var stonewallvertical = [314, 383, 40, 139]; //354 522
-var stonewallhorizontal = [374, 380, 85, 55]; //459 435
-var stonesmall = [440, 467, 22, 16]; //462 483
-var stonemedium = [371, 458, 49, 34]; //420 492
-var stonelarge = [491, 485, 61, 43]; //552 528
-var stonegiant = [572, 359, 118, 127]; //690 486
-var stonestwo = [398, 494, 61, 37]; //459 531
 
 //subfunctions 
 //function drawpicture(image, array, x, y) { //pass an image, an array format x, y, x2, y2 and then two numbers x postion and y postition
@@ -104,11 +77,12 @@ function moveTechno() {
 //the main function
 function main() {
   ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); //clear screen
-  if (situation == "walking") {
-    moveTechno();
-    drawLand();
-    drawTechno();
-    //do this drawTerrain();
+  if (situation == "walking") { //to walk, do...
+    moveTechno(); //detect keys
+    drawLand(); //draw behind land
+    detectCollisions(); //detect collisions
+    drawTechno(); //draw techno
+    drawTerrain(); //draw in front
     coords[2] = coords[3] = 0; //reset dx, dy
   } //add else if
   speedcounter = (speedcounter+1)%60;//tick the speed counter, very important
@@ -121,9 +95,11 @@ function loadAssets() {
   window.ctx = canvas.getContext('2d');
 	technospritesone = new Image();
   technospritesone.src = ('img/technosprites2.png'); //Load his sprites
-  landelementsone = new Image();
-  landelementsone.src = ('img/technoresources1.png'); //load the scenery
-  main();
+  schlattsprites = new Image();
+  schlattsprites.src = ('img/schlattsprites.png');
+  //landelementsone = new Image();
+  //landelementsone.src = ('img/technoresources1.png'); //load the scenery
+  main(); //call main function
 }
 
 //landscape functions
@@ -131,7 +107,13 @@ function drawLand(){
 	//pass
 }
 
-function technoWalk(){
+function drawTerrain(){
+  //pass
+}
+
+//techno walk functions
+function drawTechno() {
+	
   //fix this to multi direction and location and modular
   if (downPressed) {
     if (lastfunction != "technoWalkfront") {
@@ -190,13 +172,23 @@ function technoWalk(){
   }
 }
 
-function drawTechno() {
-	if (situation == "walking") {
-  	technoWalk();
-  } else {
-  	//pass
-	}
-} 
+function detectCollisions() { //detects collisions
+  if (coords[0] > 500){
+    coords[0] = 500;
+    coords[2] = 0;
+  }
+  if (coords[0] < 0) {
+    coords[0] = 0;
+    coords[2] = 0;
+  }
+  if (coords[1] > 449) {
+    coords[1] = 449;
+    coords[3] = 0;
+  }
+  if (coords[1] < 0) {
+    coords[1] = 0;
+    coords[3] = 0;
+  }
+}
 
-
-window.addEventListener("load", loadAssets); //main function
+window.addEventListener("load", loadAssets); //main function caller
