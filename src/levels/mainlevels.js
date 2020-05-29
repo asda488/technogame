4beed94676c9e760e922e116ff8a302c4c7d835b
asda488 <39116917+asda488@users.noreply.github.com>
2020-05-29 10:19:06 +0100

    Update mainlevels.js

--- src/levels/mainlevels.js
+++ src/levels/mainlevels.js
@@ -1,1 +1,20 @@
-//placeholder
+var mainlevelpictures = {"technoresourcesone":"technoresources1.png"}; //please add img/ here
+
+//the sprite numbers
+var mainleveltree = ["technoresourcesone", 482, 47, 190, 256, 60, 145, 133, 256];  //src, cx, cy, dx, dy, bx1, by1, by2, bx2 (c = picture loc, d = width etc, b = bounding box coords relative)
+
+var levela = {
+  terrain : [ //the infront stuff, with bounding boxes
+    { //a tree
+      "type" : mainleveltree,
+      "x" : 100, //x position
+      "y" : 100 //y position
+    }
+  ],
+  landscape : [// the behind stuff
+    
+  ],
+  special : [ //aaannd the infront stuff, but interactive.
+    
+  ]
+};
